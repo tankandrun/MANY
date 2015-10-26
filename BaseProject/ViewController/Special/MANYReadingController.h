ad69fb4714399c9199d56a442bf9c1cc29606b16
tankandrun <jinshundu@gmail.com>
2015-10-26 19:20:58 +0800

    阅读界面完成

---
+++ BaseProject/ViewController/Special/MANYReadingController.h
@@ -0,0 +1,13 @@
+//
+//  MANYReadingController.h
+//  MANY
+//
+//  Created by soft on 15/10/26.
+//  Copyright © 2015年 Main. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MANYReadingController : UITableViewController
+
+@end
