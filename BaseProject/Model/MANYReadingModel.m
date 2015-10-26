4d3d20ecc082b10c5a1b8c81629c2f051b679784
tankandrun <jinshundu@gmail.com>
2015-10-26 22:35:44 +0800

    2015-10-26    完成Model和NetManager

---
+++ BaseProject/Model/MANYReadingModel.m
@@ -0,0 +1,13 @@
+//
+//  MANYReadingModel.m
+//  MANY
+//
+//  Created by soft on 15/10/26.
+//  Copyright © 2015年 Main. All rights reserved.
+//
+
+#import "MANYReadingModel.h"
+
+@implementation MANYReadingModel
+
+@end
---
+++ BaseProject/Model/MANYThingModel.m
@@ -0,0 +1,13 @@
+//
+//  MANYThingModel.m
+//  MANY
+//
+//  Created by soft on 15/10/26.
+//  Copyright © 2015年 Main. All rights reserved.
+//
+
+#import "MANYThingModel.h"
+
+@implementation MANYThingModel
+
+@end
