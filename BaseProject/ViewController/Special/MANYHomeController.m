2393ec829c92bfece5cb20cb61d8a5863ae09335
tankandrun <jinshundu@gmail.com>
2015-10-26 18:29:56 +0800

    ONE的仿制品－MANY
    创建，完成首页的界面

---
+++ BaseProject/ViewController/Special/MANYHomeController.m
@@ -0,0 +1,98 @@
+//
+//  MANYHomeController.m
+//  MANY
+//
+//  Created by soft on 15/10/26.
+//  Copyright © 2015年 Main. All rights reserved.
+//
+
+#import "MANYHomeController.h"
+
+@interface MANYHomeController ()
+
+@end
+
+@implementation MANYHomeController
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    UIImageView *topLogo = [[UIImageView alloc]init];
+    topLogo.contentMode = UIViewContentModeScaleAspectFit;
+    topLogo.image = [UIImage imageNamed:@"navLogo"];
+    topLogo.frame = CGRectMake(0, 0, 100, 18);
+    self.navigationItem.titleView = topLogo;
+    
+    UIBarButtonItem *button = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"shareBtn"] style:(UIBarButtonItemStyleDone) target:nil action:nil];
+    self.navigationItem.rightBarButtonItem = button;
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.tableView.showsVerticalScrollIndicator = NO;
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    
+    return cell;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return kWindowH-64-49;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
